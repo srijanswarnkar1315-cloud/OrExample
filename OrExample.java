6f9b235b18b46127ab3765dd2c8f444aa98c87df
srijanswarnkar1315-cloud <srijanswarnkar1315@gmail.com>
2025-10-05 22:43:35 +0530

    OrExample

---
+++ OrExample.java
@@ -0,0 +1,8 @@
+public class OrExample {
+    public static void main(String[]args){
+        int a = 12;
+        int b = 10;
+        int result = a|b;
+        System.out.println("a|b="+result);
+    }    
+}
